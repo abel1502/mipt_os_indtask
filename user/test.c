042c06ffa9d35f634f21511b24e9f01f0aa70694
belyaev.aa <belyaev.aa@phystech.edu>
2022-02-09 19:16:22 +0300

    Add an easter egg

--- user/test.c
+++ user/test.c
@@ -7,7 +7,73 @@
 #include <inc/libdoom.h>
 
 
+// #define PLEASE_DRAW_SOME_PENS
+
+
+#define SCREEN_H 400
+#define SCREEN_W 640
+
+
 uint32_t *fb = NULL;
+
+
+struct vec2d {
+    double x, y;
+};
+
+struct vec2i {
+    int x, y;
+};
+
+
+void
+draw_ball(struct vec2i center, int radius, uint32_t color) {
+    for (int y = 0; y < SCREEN_H; ++y) {
+        for (int x = 0; x < SCREEN_W; ++x) {
+            struct vec2i delta = (struct vec2i){x - center.x, y - center.y};
+
+            if (delta.x * delta.x + delta.y * delta.y <= radius * radius) {
+                fb[y * SCREEN_W + x] = color;
+            }
+        }
+    }
+}
+
+void
+draw_rect(struct vec2i start, struct vec2i end, uint32_t color) {
+    if (start.x > end.x) {
+        int tmp = start.x;
+        start.x = end.x;
+        end.x = tmp;
+    }
+
+    if (start.y > end.y) {
+        int tmp = start.y;
+        start.y = end.y;
+        end.y = tmp;
+    }
+
+    start.x = MAX(start.x, 0);
+    start.y = MAX(start.y, 0);
+    end.x = MIN(end.x, SCREEN_W);
+    end.y = MIN(end.y, SCREEN_H);
+    
+
+    for (int y = start.y; y < end.y; ++y) {
+        for (int x = start.x; x < end.x; ++x) {
+            fb[y * SCREEN_W + x] = color;
+        }
+    }
+}
+
+
+void
+draw_pens(struct vec2i root, unsigned ball_radius, unsigned stem_length, unsigned stem_radius, uint32_t color) {
+    draw_ball((struct vec2i){root.x - ball_radius, root.y}, ball_radius, color);
+    draw_ball((struct vec2i){root.x + ball_radius, root.y}, ball_radius, color);
+    draw_rect((struct vec2i){root.x - stem_radius, root.y + ball_radius / 2}, (struct vec2i){root.x + stem_radius, root.y - stem_length}, color);
+    draw_ball((struct vec2i){root.x, root.y - stem_length}, stem_radius, color);
+}
 
 
 void
@@ -24,11 +90,17 @@ umain(int argc, char **argv) {
     assert(res >= 0);
     printf("fb = %p\n", fb);
 
-    for (unsigned y = 0; y < 400; ++y) {
-        for (unsigned x = 0; x < 640; ++x) {
-            fb[y * 640 + x] = 0xFF0000FF;
-        }
-    }
+    // for (unsigned y = 0; y < SCREEN_H; ++y) {
+    //     for (unsigned x = 0; x < SCREEN_W; ++x) {
+    //         fb[y * SCREEN_W + x] = 0x000000FF;
+    //     }
+    // }
+
+    draw_rect((struct vec2i){0, 0}, (struct vec2i){SCREEN_W, SCREEN_H}, 0xFF000000);
+
+    #ifdef PLEASE_DRAW_SOME_PENS
+    draw_pens((struct vec2i){SCREEN_W / 2, SCREEN_H - 50}, 25, 200, 10, 0xFF0000FF);
+    #endif
     
     res = sys_virtiogpu_flush();
     assert(res >= 0);
